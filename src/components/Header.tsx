cb70331cc43c717986cf64929f2799c0052c9986
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-22 21:45:17 +0800

    feat: add login button to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Header() {
   return (
-    <div className="min-h-10 bg-black flex-auto">
+    <div className="min-h-10 bg-black flex">
       <h1 className="text-white">Loomi</h1>
+      <div>
+        <Button variant="outline">Login</Button>
+      </div>
     </div>
   );
 }
