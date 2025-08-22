ad41f400e7bff3d6d48bc078da154bdf170f93de
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-22 21:41:40 +0800

    feat: integrate Landing component into App and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Landing from "./pages/Landing";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
+    <div className="min-h-screen flex-auto bg-white">
+      <div>
+        <Landing></Landing>
+      </div>
       <h1 className="text-5xl font-bold text-green-400">
         Tailwind is working ✅
       </h1>
