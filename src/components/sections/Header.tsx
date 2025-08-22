fe2a0a377dc7f723105ea23cd2d56e8e1fb7419b
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-22 23:19:46 +0800

    feat: create Header component with login and sign-up buttons

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <div className="min-h-15 p-2 flex items-center">
+    <div className="min-h-15 p-2 flex items-center border-b-0 ">
       <div className="mx-20 w-full flex items-center justify-between">
         <div className="flex items-center">
           <p>Loomi</p>
