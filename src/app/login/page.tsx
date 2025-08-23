cd71c68cafc28cbe4f1758b0bf1c659d501c963c
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 00:57:19 +0800

    Auto-commit page.tsx: 2025-08-24 00:57:19

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
-export default function Login() {
+import LoginPopover from "@/components/LoginPopover";
+
+export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-white h-[50vh] w-[50vh] rounded-2xl"></div>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <LoginPopover />
     </div>
   );
 }
