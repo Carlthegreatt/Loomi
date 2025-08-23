513f46fc2778e7f4d301f5397a1f94a778bdb1b9
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-23 12:11:58 +0800

    feat: integrate HowItWorks component into Landing page for enhanced user guidance

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/sections/Header";
 import Hero from "@/components/sections/Hero";
+import HowItWorks from "@/components/sections/HowItWorks";
 
 export default function Landing() {
   return (
     <>
       <Header />
       <Hero />
+      <HowItWorks></HowItWorks>
     </>
   );
 }
