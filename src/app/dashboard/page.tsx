959ba163bc1efa10c67703f9d6f1a107a95bbb1c
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 00:24:39 +0800

    Auto-commit page.tsx: 2025-08-24 00:24:39

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50">
-              <div className="items-center">fdsf</div>
+              <div className="items-center flex">fdsf</div>
             </div>
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
