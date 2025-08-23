a345b5094177e2e42e8655eed4e480f955f97963
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-23 14:36:17 +0800

    feat: update webpack hot-update files and hashes for improved development experience

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40e69e9504df47ce"; }
+/******/ 		__webpack_require__.h = function() { return "b1aa6e55139946d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
