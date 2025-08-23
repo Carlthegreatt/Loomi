29a8ddc996e7642a2963852f8feee5acb22d18d6
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-23 12:11:36 +0800

    chore: add Inter font styles for improved typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/inter/400.css"; // defaults to weight 400
+import "@fontsource/inter/500.css"; // optional: specific weights
+import "@fontsource/inter/700.css";
+
 import "./index.css";
 import App from "./App.tsx";
 
