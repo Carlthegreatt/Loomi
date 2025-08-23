93fbd6b28722619ee8a1c57a837d83af806b6537
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 01:23:22 +0800

    Auto-commit LoginPopover.tsx: 2025-08-24 01:23:22

--- src/components/sections/LoginPopover.tsx
+++ src/components/sections/LoginPopover.tsx
@@ -39,7 +39,7 @@ export default function LoginModal() {
               />
               <div>
                 <h1 className="text-white text-sm">Password</h1>
-                <button className="text-white text-sm grid flex">
+                <button className="text-white text-sm grid flex-auto">
                   Forgot Passowrd?
                 </button>
               </div>
