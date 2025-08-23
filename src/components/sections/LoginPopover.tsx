712de48447868643e9e0312967f76249eb2dfdf8
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 01:19:15 +0800

    Auto-commit LoginPopover.tsx: 2025-08-24 01:19:15

--- src/components/sections/LoginPopover.tsx
+++ src/components/sections/LoginPopover.tsx
@@ -44,7 +44,7 @@ export default function LoginModal() {
               />
               <button
                 type="submit"
-                className="w-full bg-white text-black py-2 rounded-md hover:bg-slate-600"
+                className="w-full bg-white text-black py-2 rounded-md hover:bg-neutral-600"
               >
                 Sign In
               </button>
