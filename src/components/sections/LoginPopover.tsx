fce3d84c13b95f127f0a601894283b2e4b5d158b
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 01:01:22 +0800

    Auto-commit LoginPopover.tsx: 2025-08-24 01:01:22

--- src/components/sections/LoginPopover.tsx
+++ src/components/sections/LoginPopover.tsx
@@ -21,7 +21,7 @@ export default function LoginModal() {
         <div className="fixed inset-0 z-50 flex items-center justify-center">
           {/* Background overlay */}
           <div
-            className="absolute inset-0 bg-black bg-opacity-50 blur-md"
+            className="absolute inset-0 bg-black bg-opacity-50 blur-sm"
             onClick={() => setOpen(false)} // click outside closes
           />
 
