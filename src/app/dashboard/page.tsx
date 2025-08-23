f6dfc41b459ad9de2a180a45edc9c7573a25ab32
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-23 19:49:32 +0800

    Auto-commit page.tsx: 2025-08-23 19:49:32

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -25,8 +25,9 @@ export default function Page() {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Search></Search>
+
             <Input placeholder="Search" className="h-7 text-sm flex"></Input>
+            <Search></Search>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
