c71ff670588a1fb21c7f3177850575dd6ba203fc
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 01:11:50 +0800

    Auto-commit LoginPopover.tsx: 2025-08-24 01:11:50

--- src/components/sections/LoginPopover.tsx
+++ src/components/sections/LoginPopover.tsx
@@ -38,7 +38,6 @@ export default function LoginModal() {
               />
               <input
                 type="password"
-                placeholder="Password"
                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <button
