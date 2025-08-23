e8c019d157434bff8e679a9cf7d13ba1fef60abb
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 00:29:25 +0800

    Auto-commit page.tsx: 2025-08-24 00:29:25

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
           </div>
           <div className="grid auto-rows-min gap-4 md:grid-cols-2">
             <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="min-h-[100vh] flex-auto rounded-xl bg-muted/50 md:min-h-min" />
+            <div className="aspect-video rounded-xl bg-muted/50 md:min-h-min" />
           </div>
         </div>
       </SidebarInset>
