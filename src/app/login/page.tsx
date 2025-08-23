e612d142e6b94e0c3e20f2ab2b540ce401ca611c
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 00:49:59 +0800

    Auto-commit page.tsx: 2025-08-24 00:49:59

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
   return (
-    <div className="flex-shrink-0 place-items-center">
+    <div className="flex-auto place-items-center">
       <div className="bg-white h-12 w-12"></div>
     </div>
   );
