5fb48651bbdb691257b584fd23922609c868e74d
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-24 15:01:24 +0800

    Auto-commit Header.tsx: 2025-08-24 15:01:24

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import LoginPopover from "@/components/sections/LoginPopover";
+import LoginPopover from "@/components/sections/SigninPopover";
 
 export default function Header() {
   return (
