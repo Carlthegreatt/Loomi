29ddfd4e0190589d6be18d60bf4ce03e57f1cdbe
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-25 23:30:37 +0800

    Auto-commit SignupPopover.tsx: 2025-08-25 23:30:37

--- src/components/sections/SignupPopover.tsx
+++ src/components/sections/SignupPopover.tsx
@@ -56,16 +56,6 @@ export default function SignupModal() {
                 className="w-full text-sm bg-[#212121] text-white px-3 py-2 border rounded-md focus:outline-1 "
               />
 
-              <Checkbox
-                checked={}
-                onCheckedChange={(checked) => {
-                  return checked
-                    ? field.onChange([...field.value, item.id])
-                    : field.onChange(
-                        field.value?.filter((value) => value !== item.id)
-                      );
-                }}
-              />
               <button
                 type="submit"
                 className="w-full text-sm bg-white text-black py-2 rounded-md hover:bg-neutral-300"
