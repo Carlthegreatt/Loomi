b81ce6f12dbb7c9ede3897a30d4f31337474e2cf
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-25 23:29:16 +0800

    Auto-commit SignupPopover.tsx: 2025-08-25 23:29:16

--- src/components/sections/SignupPopover.tsx
+++ src/components/sections/SignupPopover.tsx
@@ -56,8 +56,7 @@ export default function SignupModal() {
                 className="w-full text-sm bg-[#212121] text-white px-3 py-2 border rounded-md focus:outline-1 "
               />
 
-              <Checkbox checked>fd</Checkbox>
-
+              <input type="checkbox"></input>
               <button
                 type="submit"
                 className="w-full text-sm bg-white text-black py-2 rounded-md hover:bg-neutral-300"
