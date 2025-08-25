f1dc59cf22263d069ff96af7c3eaf6bddc9f1dc4
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-25 23:31:52 +0800

    Auto-commit SignupPopover.tsx: 2025-08-25 23:31:52

--- src/components/sections/SignupPopover.tsx
+++ src/components/sections/SignupPopover.tsx
@@ -56,7 +56,7 @@ export default function SignupModal() {
                 className="w-full text-sm bg-[#212121] text-white px-3 py-2 border rounded-md focus:outline-1 "
               />
 
-              <input type="checkbox" placeholder="fds"></input>
+              <input type="checkbox">Accept terms and conditions</input>
 
               <button
                 type="submit"
