51dda769f33366889a7d924d622ccfec5d507bd7
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-08-28 11:08:36 +0800

    chore(Loomi): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-08-28 03:08:36 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/app/study/pomodoro/page.tsx

--- src/app/study/pomodoro/page.tsx
+++ src/app/study/pomodoro/page.tsx
@@ -1,9 +1,7 @@
 export default function Pomodoro() {
   return (
-    <div className="flex justify-center h-screen p-7">
-      <div className="w-4/5 max-w-lg max-h-[40vh] text-center rounded-2xl bg-slate-700">
-        box
-      </div>
+    <div className="flex justify-center h-screen p-10">
+      <div className="w-4/5 max-w-lg max-h-[50vh] text-center rounded-2xl bg-slate-700"></div>
     </div>
   );
 }
